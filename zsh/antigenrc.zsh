0458e94b85bf6ec34b2f414e424df37959d12619
mvwicky <mvanwickle@gmail.com>
2024-02-18 09:36:50 -0800

    zsh: Remove unused antigen packages

--- zsh/antigenrc.zsh
+++ zsh/antigenrc.zsh
@@ -2,19 +2,19 @@
 antigen use oh-my-zsh
 
 antigen bundle rust
-# antigen bundle django
 antigen bundle gitfast
 antigen bundle httpie
-antigen bundle npm
 antigen bundle yarn
 antigen bundle ripgrep
+# antigen bundle django
+# antigen bundle npm
 # antigen bundle docker
 # antigen bundle docker-compose
 
 antigen bundle zsh-users/zsh-completions
+antigen bundle zdharma-continuum/fast-syntax-highlighting
 # antigen bundle zsh-users/zsh-autosuggestions
 # antigen bundle djui/alias-tips
-antigen bundle zdharma-continuum/fast-syntax-highlighting
 # antigen bundle zsh-users/zsh-syntax-highlighting
 
 antigen apply
