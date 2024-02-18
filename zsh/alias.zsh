6c36a6eeafde46b887df80b55e1b3e3ae23fa223
mvwicky <mvanwickle@gmail.com>
2024-02-18 09:36:25 -0800

    zsh: Update some aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,16 +4,16 @@ alias dj="poetry run python manage.py"
 alias p="poetry"
 alias py="python"
 alias pym="python manage.py"
-alias pup='python -m pip install -U pip setuptools wheel'
-alias pupx='python -m pip install -U pip setuptools wheel pipx'
-alias pip-old='pip list --outdated'
-alias pold='poetry show --outdated'
+alias pup="python -m pip install -U pip"
+alias pupx="python -m pip install -U pip pipx"
+alias pip-old="pip list --outdated"
+alias pold="poetry show --outdated"
 alias sm='smerge $PWD'
 
 alias ga="git add"
 alias gp="git push --verbose"
 alias gc="git commit"
-alias st='git status'
+alias st="git status"
 
 alias yas="yarn --silent"
 alias yc="yarn config"
@@ -31,18 +31,19 @@ else
   alias ll="ls -lah"
 fi
 
+alias prettiert='prettier --plugin "$(yg dir)"/node_modules/prettier-plugin-toml/lib/index.js'
+
 if command -v gmake 1> /dev/null 2>&1; then
   alias make='gmake'
 fi
 
 alias zshconfig='${EDITOR} ~/.zshrc'
-alias histedit='${EDITOR} ~/.zsh_history'
+alias histedit='${EDITOR} ${HISTFILE}'
 alias dustr='dust -r'
 
 alias ffprobe='ffprobe -hide_banner'
 alias ffmpeg='ffmpeg -hide_banner'
 alias px='pipxr'
-alias batt='bat --terminal-width=$(tput cols)'
 alias yupl='yarn upgrade -L'
 alias yg='yarn global'
 alias g='git'
