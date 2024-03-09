f94ca2f1a4113ce142e2d0e10667930841ca23d2
mvwicky <mvanwickle@gmail.com>
2024-03-09 10:42:01 -0800

    nvim: Add `neo-tree`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -90,6 +90,22 @@ require("lazy").setup({
     "nvim-telescope/telescope.nvim",
     dependencies = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          hijack_netrw_behavior = "open_default",
+        },
+      })
+    end,
+  },
   "raimondi/delimitmate",
   "tpope/vim-commentary",
   "tpope/vim-surround",
