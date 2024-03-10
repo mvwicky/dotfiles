f71895393442e3dc34de09e5c4a11bb49e29308d
mvwicky <mvanwickle@gmail.com>
2024-03-09 17:54:29 -0800

    nvim: Make more plugins lazy

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -41,19 +41,19 @@ require("lazy").setup({
       require("tokyonight").setup({ style = "moon" })
     end,
   },
-  "sheerun/vim-polyglot",
+  { "sheerun/vim-polyglot", event = "VeryLazy" },
   { "rust-lang/rust.vim", ft = { "rust" } },
   { "ekalinin/dockerfile.vim", ft = { "dockerfile" } },
-  "glench/vim-jinja2-syntax",
+  { "glench/vim-jinja2-syntax", enabled = false },
   { "cespare/vim-toml", ft = { "toml" } },
   { "vim-scripts/django.vim", event = { "BufEnter *.djt", "BufEnter *.html" } },
-  "pangloss/vim-javascript",
-  "kevinoid/vim-jsonc",
+  { "pangloss/vim-javascript", ft = { "javascript", "javascriptreact" } },
   { "vim-python/python-syntax", ft = { "python" } },
   "fladson/vim-kitty",
-  "NoahTheDuke/vim-just",
+  { "NoahTheDuke/vim-just", event = { "BufEnter justfile", "BufEnter .justfile" } },
   {
     "lukas-reineke/indent-blankline.nvim",
+    event = "VeryLazy",
     main = "ibl",
     opts = { indent = { char = { "|", "¦", "┆", "┊" } } },
   },
@@ -106,12 +106,13 @@ require("lazy").setup({
       })
     end,
   },
-  "raimondi/delimitmate",
-  "tpope/vim-commentary",
-  "tpope/vim-surround",
-  "nvim-tree/nvim-web-devicons",
+  { "raimondi/delimitmate", event = "VeryLazy" },
+  { "tpope/vim-commentary", event = "VeryLazy" },
+  { "tpope/vim-surround", event = "VeryLazy" },
+  { "nvim-tree/nvim-web-devicons", event = "VeryLazy" },
   {
     "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
     dependencies = { { "nvim-tree/nvim-web-devicons" } },
     config = function()
       require("lualine").setup({
@@ -120,9 +121,10 @@ require("lazy").setup({
       })
     end,
   },
-  "luochen1990/rainbow",
+  { "luochen1990/rainbow", event = { "BufReadPre", "BufNewFile" } },
   {
     "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
@@ -131,6 +133,7 @@ require("lazy").setup({
           "markdown",
           "markdown_inline",
           "lua",
+          "vimdoc",
         },
         highlight = { enable = true },
         -- indent = { enable = true },
@@ -158,9 +161,14 @@ require("lazy").setup({
   },
   {
     "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("gitsigns").setup()
     end,
   },
-  { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+  },
 })
