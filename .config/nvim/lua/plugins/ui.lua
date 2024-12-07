385e27507a5118456614318b39f9d6a48d2d260c
mvwicky <mvanwickle@gmail.com>
2024-12-07 12:41:26 -0800

    nvim: Add `which-key` plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -93,4 +93,18 @@ return {
       { "<leader>tt", "<cmd>:Trouble diagnostics toggle<cr>" },
     },
   },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = { preset = "modern" },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
 }
