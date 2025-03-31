a23b42c2866a64e7de39d9aa2c3a4a77a9f54592
mvwicky <mvanwickle@gmail.com>
2025-03-31 15:52:23 -0700

    Add `eget` helper script

---
+++ eget.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install eget tools
+
+eget --upgrade-only zyedidia/knit
+eget --upgrade-only boyter/scc
+eget --upgrade-only denoland/denokv
+eget --upgrade-only kristoff-it/superhtml
+eget --upgrade-only frectonz/sql-studio
