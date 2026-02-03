3cee88622f22756e5e4d5cb0788159f9e7fdf657
mvwicky <mvanwickle@gmail.com>
2026-02-03 10:56:39 -0800

    nvim: Update LSP config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -88,10 +88,7 @@ return {
       --Enable (broadcasting) snippet capability for completion
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-      require("lspconfig").jsonls.setup({
-        capabilities = capabilities,
-      })
+      vim.lsp.config("jsonls", { capabilities = capabilities })
     end,
   },
 }
